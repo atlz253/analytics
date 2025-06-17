bab1bb56848cc44a8e054e7e64091daea30d9337
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2025-06-17 16:10:31 +0300

    добавил расширения к импортируемым файлам

--- packages/api/tests/ping.test.ts
+++ packages/api/tests/ping.test.ts
@@ -1,8 +1,8 @@
 import { afterEach, beforeEach, describe, expect, test } from "vitest";
-import { API } from "../src";
-import { Ping } from "../../ping/src";
-import { localhost } from "./utils/address";
-import { ImmutableEventEmitter } from "../../shared/src/ImmutableEventEmitter";
+import { API } from "../src/index.js";
+import { Ping } from "../../ping/src/index.js";
+import { localhost } from "./utils/address.js";
+import { ImmutableEventEmitter } from "../../shared/src/ImmutableEventEmitter.js";
 
 describe("/ping", () => {
   let events = new ImmutableEventEmitter();
